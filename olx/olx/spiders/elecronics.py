d07c3df115886f0c7011e3a5803a305901c39e6b
taylorzhangyx <taylorzhangyx@yahoo.com>
2019-03-12 23:03:01 -0700

    fetch all item bby title

--- olx/olx/spiders/elecronics.py
+++ olx/olx/spiders/elecronics.py
@@ -1,19 +1,21 @@
 # -*- coding: utf-8 -*-
+import scrapy
 from scrapy.spiders import CrawlSpider, Rule
 from scrapy.linkextractors import LinkExtractor
 class ElectronicsSpider(CrawlSpider):
     name = "electronics"
-    allowed_domains = ["www.olx.com.pk"]
+    allowed_domains = ["sfbay.craigslist.org"]
     start_urls = [
-        'https://www.olx.com.pk/computers-accessories/',
-        'https://www.olx.com.pk/tv-video-audio/',
-        'https://www.olx.com.pk/games-entertainment/'
+        'https://sfbay.craigslist.org/d/furniture/search/fua'
     ]
 
     rules = (
-        Rule(LinkExtractor(allow=(), restrict_css=('.pageNextPrev',)),
+        Rule(LinkExtractor(allow=(), restrict_css=('.next',)),
              callback="parse_item",
              follow=True),)
 
     def parse_item(self, response):
         print('Processing..' + response.url)
+        itemsTitle = response.css('.result-title::text').getall()
+        for item in itemsTitle:
+            yield scrapy.Request(item, callback = self.processItem)
